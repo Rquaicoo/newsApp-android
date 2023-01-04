4477bf89fd95b5088d2f87ed883c1cebebd78a20
Rquaicoo <russellquaicoo1@gmail.com>
2023-01-04 16:52:20 +0000

    setup for authentication ui

--- app/src/main/java/com/example/newsapp_android/ui/authentication/Login.kt
+++ app/src/main/java/com/example/newsapp_android/ui/authentication/Login.kt
@@ -1,2 +1,8 @@
 package com.example.newsapp_android.ui.authentication
 
+import androidx.compose.runtime.Composable
+
+@Composable
+fun Login() {
+
+}
--- app/src/main/java/com/example/newsapp_android/ui/authentication/Register.kt
+++ app/src/main/java/com/example/newsapp_android/ui/authentication/Register.kt
@@ -1,2 +1,9 @@
 package com.example.newsapp_android.ui.authentication
 
+import androidx.compose.runtime.Composable
+import com.example.newsapp_android.Register
+
+@Composable
+fun Register() {
+
+}
