130962e92bfb3c7a9eb2c07b1a2dc8f6c60e1a94
Rquaicoo <russellquaicoo1@gmail.com>
2023-01-04 16:54:26 +0000

    set up navigation for the app

---
+++ app/src/main/java/com/example/newsapp_android/NewsAppDestinations.kt
@@ -0,0 +1,31 @@
+package com.example.newsapp_android
+
+import androidx.compose.runtime.Composable
+import com.example.newsapp_android.ui.authentication.Login
+import com.example.newsapp_android.ui.authentication.Register
+
+interface NewsAppDestination {
+    val route : String
+    val screen : @Composable () -> Unit
+}
+
+object Onboarding : NewsAppDestination {
+    override val route = "onboarding"
+    override val screen : @Composable () -> Unit = { OnboardingComponent() }
+}
+
+object Register : NewsAppDestination {
+    override val route = "register"
+    override val screen : @Composable () -> Unit = { Login() }
+}
+
+object Login : NewsAppDestination {
+    override val route = "login"
+    override val screen : @Composable () -> Unit = { Register() }
+}
+
+object Home : NewsAppDestination {
+    override val route = "home"
+    /*TODO: To be modified*/
+    override val screen : @Composable () -> Unit = { OnboardingComponent() }
+}
