ff8a114318e3dde6868bd19e2b6e1f8ac8d1c15a
Rquaicoo <russellquaicoo1@gmail.com>
2023-05-05 01:22:23 +0000

    move trending posts to top of screen

--- app/src/main/java/com/example/newsapp_android/components/HomeScreenComponents.kt
+++ app/src/main/java/com/example/newsapp_android/components/HomeScreenComponents.kt
@@ -1,7 +1,7 @@
 package com.example.newsapp_android.components
 
-import androidx.compose.foundation.background
-import androidx.compose.foundation.border
+import android.content.Context
+import androidx.compose.foundation.*
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.shape.RoundedCornerShape
@@ -13,16 +13,217 @@ import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import coil.compose.rememberAsyncImagePainter
 import com.example.newsapp_android.R
+import com.example.newsapp_android.dataclasses.PostsModel
 import com.example.newsapp_android.ui.theme.NewsAppandroidTheme
+import com.example.newsapp_android.utilities.openLink
+import java.util.*
+
+@Composable
+fun NewsComponent(post: PostsModel, context: Context) {
+    Box(
+        modifier = Modifier
+            .height(329.dp)
+            .fillMaxWidth()
+            .padding(top = 20.dp, bottom = 20.dp)
+            .clickable(
+                onClick = {
+                    openLink(context, post.url)
+                }
+            )
+
+    ) {
+        Image(
+            painter = rememberAsyncImagePainter(post.image),
+            contentDescription = null,
+            contentScale = ContentScale.Crop,
+            modifier = Modifier
+                .matchParentSize()
+                .clip(shape = RoundedCornerShape(10.dp)),
+        )
+        Box(
+            modifier = Modifier
+                .padding(horizontal = 20.dp, vertical = 25.dp)
+                .background(
+                    color = Color(0xA4, 0xB6, 0xC5),
+                    shape = RoundedCornerShape(20.dp)
+                )
+                .height(28.dp)
+                .widthIn(min = 10.dp)
+                .padding(horizontal = 4.dp, vertical = 1.dp)
+        ) {
+            Text(
+                post.category.replaceFirstChar {
+                    it
+                    if (it.isLowerCase()) it.titlecase(
+                        Locale.getDefault()
+                    ) else it.toString()
+                },
+                lineHeight = 17.sp,
+                fontStyle = FontStyle.Normal,
+                fontFamily = FontFamily(Font(R.font.arial)),
+                color = Color.White,
+                modifier = Modifier.padding(
+                    horizontal = 10.dp,
+                    vertical = 1.dp
+                )
+            )
+        }
+
+
+
+        Column(
+            modifier = Modifier
+                .align(Alignment.BottomEnd)
+                .background(
+                    color = Color(0xFF, 0xFF, 0xFF, 0x99),
+                    shape = RoundedCornerShape(0.dp, 0.dp, 10.dp, 10.dp)
+                )
+        ) {
+            Text(
+                post.author.name,
+                lineHeight = 15.sp,
+                fontStyle = FontStyle.Normal,
+                fontFamily = FontFamily(Font(R.font.arial)),
+                color = Color(0x4E, 0x4E, 0x4E),
+                modifier = Modifier.padding(
+                    horizontal = 5.dp,
+                    vertical = 1.dp
+                )
+            )
+
+            Text(
+                post.title,
+                lineHeight = 17.sp,
+                fontStyle = FontStyle.Normal,
+                fontWeight = FontWeight.Bold,
+                fontFamily = FontFamily(Font(R.font.arial)),
+                color = Color.Black,
+                modifier = Modifier.padding(
+                    horizontal = 5.dp,
+                    vertical = 1.dp
+                )
+            )
+
+            Row(modifier = Modifier) {
+                Spacer(Modifier.weight(1f))
+
+                Text(
+                    "20 hours ago",
+                    lineHeight = 17.sp,
+                    fontStyle = FontStyle.Normal,
+                    fontFamily = FontFamily(Font(R.font.arial)),
+                    color = Color.Black,
+                    modifier = Modifier.padding(
+                        horizontal = 5.dp,
+                        vertical = 1.dp
+                    )
+                )
+            }
+        }
+    }
+}
+
+
+@Composable
+fun TrendingItemComponent(post: PostsModel, context: Context, modifier: Modifier = Modifier) {
+    /*Sample Trending Post*/
+
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .background(color = Color.White, shape = RoundedCornerShape(10.dp))
+            .height(129.dp)
+            .clickable { openLink(context, post.url) }
+    ) {
+        Column(modifier = Modifier.width(200.dp)) {
+            Row(modifier = Modifier.padding(horizontal = 15.dp, vertical = 5.dp)) {
+                Image(
+                    painter = rememberAsyncImagePainter(post.author.image),
+                    contentDescription = null,
+                    contentScale = ContentScale.FillHeight,
+                    modifier = Modifier
+                        .padding(top = 4.dp, bottom = 1.dp)
+                        .width(14.dp)
+                        .height(14.dp)
+                        .clip(shape = CircleShape)
+
+                )
+
+                Text(
+                    post.author.name,
+                    lineHeight = 15.sp,
+                    fontStyle = FontStyle.Normal,
+                    fontWeight = FontWeight.Bold,
+                    fontFamily = FontFamily(Font(R.font.arial)),
+                    color = Color(0x4E, 0x4E, 0x4E),
+                    modifier = Modifier.padding(horizontal = 10.dp, vertical = 1.dp)
+                )
+            }
+
+            Text(
+                post.title,
+                lineHeight = 15.sp,
+                fontStyle = FontStyle.Normal,
+                fontWeight = FontWeight.Bold,
+                fontFamily = FontFamily(Font(R.font.arial)),
+                color = Color(0x4E, 0x4E, 0x4E),
+                modifier = Modifier.padding(horizontal = 15.dp),
+                overflow = TextOverflow.Ellipsis
+
+            )
+
+            Row(Modifier.padding(vertical = 5.dp, horizontal = 20.dp),verticalAlignment = Alignment.CenterVertically) {
+                Canvas(
+                    modifier = Modifier
+                        .padding(start = 8.dp, end = 8.dp)
+                        .size(6.dp)
+                ) {
+                    drawCircle(Color(0x4E, 0x4E, 0x4E))
+                }
+
+                Text(
+                    "Yesterday",
+                    lineHeight = 15.sp,
+                    fontStyle = FontStyle.Normal,
+                    fontWeight = FontWeight.Bold,
+                    fontFamily = FontFamily(Font(R.font.arial)),
+                    color = Color(0x4E, 0x4E, 0x4E),
+                    modifier = Modifier.padding(horizontal = 10.dp)
+                )
+            }
+
+        }
+
+        Spacer(modifier.weight(1f))
+
+        Image(
+            painter = rememberAsyncImagePainter(post.image),
+            contentDescription = null,
+            contentScale = ContentScale.Crop,
+            modifier = Modifier
+                .width(102.dp)
+                .fillMaxHeight()
+                .clip(shape = RoundedCornerShape(0.dp, 10.dp, 10.dp, 0.dp))
+        )
+    }
+}
 
 val sections = listOf(R.string.news, R.string.sports, R.string.politics, R.string.tech)
 
@@ -81,7 +282,6 @@ fun SectionsComponent(modifier: Modifier = Modifier) {
     }
 
 }
-
 
 
 @Preview(widthDp = 360, heightDp = 100, showBackground = true)
--- app/src/main/java/com/example/newsapp_android/ui/main/HomeScreen.kt
+++ app/src/main/java/com/example/newsapp_android/ui/main/HomeScreen.kt
@@ -9,8 +9,10 @@ import androidx.browser.customtabs.CustomTabColorSchemeParams
 import androidx.browser.customtabs.CustomTabsIntent
 import androidx.compose.foundation.*
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.*
@@ -34,11 +36,14 @@ import androidx.core.content.ContextCompat
 import coil.compose.rememberAsyncImagePainter
 import com.example.newsapp_android.R
 import com.example.newsapp_android.RetrofitClient
+import com.example.newsapp_android.components.NewsComponent
 import com.example.newsapp_android.components.SectionComponent
+import com.example.newsapp_android.components.TrendingItemComponent
 import com.example.newsapp_android.dataclasses.PostsModel
 import com.example.newsapp_android.interfaces.PostsApi
 import com.example.newsapp_android.ui.theme.NewsAppandroidTheme
 import com.example.newsapp_android.utilities.openLink
+import kotlinx.coroutines.launch
 import java.util.*
 
 
@@ -55,16 +60,18 @@ fun HomeScreen(modifier: Modifier = Modifier) {
 
     LaunchedEffect(Unit) {
         postsLoading = true
-        posts = SendRequest(
-            OnSuccess = {
-            postsLoading= false
-                        },
-            OnFailure = {
-                postsLoading = false
-                        }
-        )
+        scope.launch {
+            posts = SendRequest(
+                OnSuccess = {
+                    postsLoading= false
+                },
+                OnFailure = {
+                    postsLoading = false
+                }
+            )
 
-        Log.d("Posts", posts.toString())
+            Log.d("Posts", posts.toString())
+        }
     }
 
     Surface(modifier = modifier.background(color = Color(0xF1, 0xF5, 0xF7))) {
@@ -77,7 +84,6 @@ fun HomeScreen(modifier: Modifier = Modifier) {
                     .verticalScroll(rememberScrollState())
             ) {
                 /*Greeting Section*/
-                Column(modifier = Modifier) {
                     Row(
                         modifier = Modifier.padding(3.dp),
                         horizontalArrangement = Arrangement.SpaceBetween
@@ -117,9 +123,11 @@ fun HomeScreen(modifier: Modifier = Modifier) {
                         modifier = Modifier.padding(top = 1.dp, bottom = 1.dp)
                     )
 
+
+
                     LazyRow(
                         modifier = Modifier
-                            .height(100.dp)
+                            .height(70.dp)
                             .fillMaxWidth(), horizontalArrangement = Arrangement.spacedBy(8.dp)
                     ) {
                         items(com.example.newsapp_android.components.sections) { item ->
@@ -130,214 +138,48 @@ fun HomeScreen(modifier: Modifier = Modifier) {
                     /*Loading spinner*/
                     if(postsLoading && posts == null) {
                         CircularProgressIndicator(
-                            modifier = Modifier.size(30.dp)
+                            modifier = Modifier
+                                .size(30.dp)
                                 .align(Alignment.CenterHorizontally),
                             color = Color(0xF6, 0x76, 0x00)
                         )
                     }
 
-                    /*sample post*/
+                    /*trending posts*/
                     if (!postsLoading && posts != null) {
-                        for (post in posts!!) {
-                            Box(
-                                modifier = Modifier
-                                    .height(329.dp)
-                                    .fillMaxWidth()
-                                    .padding(top = 20.dp, bottom = 20.dp)
-                                    .clickable(
-                                        onClick = {
-                                            openLink(context, post.url)
-                                        }
-                                    )
+                        Text(
+                            "Trending",
+                            lineHeight = 16.sp,
+                            fontStyle = FontStyle.Normal,
+                            fontWeight = FontWeight.Bold,
+                            fontFamily = FontFamily(Font(R.font.arial)),
+                            color = Color.Black,
+                            modifier = Modifier.padding(top = 5.dp, bottom = 20.dp)
+                        )
 
-                            ) {
-                                Image(
-                                    painter = rememberAsyncImagePainter(post.image),
-                                    contentDescription = null,
-                                    contentScale = ContentScale.Crop,
-                                    modifier = Modifier
-                                        .matchParentSize()
-                                        .clip(shape = RoundedCornerShape(10.dp)),
-                                )
-                                Box(
-                                    modifier = Modifier
-                                        .padding(horizontal = 20.dp, vertical = 25.dp)
-                                        .background(
-                                            color = Color(0xA4, 0xB6, 0xC5),
-                                            shape = RoundedCornerShape(20.dp)
-                                        )
-                                        .height(28.dp)
-                                        .widthIn(min = 10.dp)
-                                        .padding(horizontal = 4.dp, vertical = 1.dp)
-                                ) {
-                                    Text(
-                                        post.category.replaceFirstChar {
-                                            if (it.isLowerCase()) it.titlecase(
-                                                Locale.getDefault()
-                                            ) else it.toString()
-                                        },
-                                        lineHeight = 17.sp,
-                                        fontStyle = FontStyle.Normal,
-                                        fontFamily = FontFamily(Font(R.font.arial)),
-                                        color = Color.White,
-                                        modifier = Modifier.padding(
-                                            horizontal = 10.dp,
-                                            vertical = 1.dp
-                                        )
-                                    )
-                                }
+                        LazyRow(modifier = Modifier.fillMaxWidth(), horizontalArrangement = Arrangement.spacedBy(8.dp)) {
+                            items(posts!!) { post ->
+                                TrendingItemComponent(post = post, context = context)
+                            }
+                        }
+                    }
 
-
-
-                                Column(
-                                    modifier = Modifier
-                                        .align(Alignment.BottomEnd)
-                                        .background(
-                                            color = Color(0xFF, 0xFF, 0xFF, 0x99),
-                                            shape = RoundedCornerShape(0.dp, 0.dp, 10.dp, 10.dp)
-                                        )
-                                ) {
-                                    Text(
-                                        post.author.name,
-                                        lineHeight = 15.sp,
-                                        fontStyle = FontStyle.Normal,
-                                        fontFamily = FontFamily(Font(R.font.arial)),
-                                        color = Color(0x4E, 0x4E, 0x4E),
-                                        modifier = Modifier.padding(
-                                            horizontal = 5.dp,
-                                            vertical = 1.dp
-                                        )
-                                    )
-
-                                    Text(
-                                        post.title,
-                                        lineHeight = 17.sp,
-                                        fontStyle = FontStyle.Normal,
-                                        fontWeight = FontWeight.Bold,
-                                        fontFamily = FontFamily(Font(R.font.arial)),
-                                        color = Color.Black,
-                                        modifier = Modifier.padding(
-                                            horizontal = 5.dp,
-                                            vertical = 1.dp
-                                        )
-                                    )
-
-                                    Row(modifier = Modifier) {
-                                        Spacer(Modifier.weight(1f))
-
-                                        Text(
-                                            "20 hours ago",
-                                            lineHeight = 17.sp,
-                                            fontStyle = FontStyle.Normal,
-                                            fontFamily = FontFamily(Font(R.font.arial)),
-                                            color = Color.Black,
-                                            modifier = Modifier.padding(
-                                                horizontal = 5.dp,
-                                                vertical = 1.dp
-                                            )
-                                        )
-                                    }
-                                }
+                    /*user feed*/
+                    if (!postsLoading && posts != null) {
+                        LazyColumn(modifier = Modifier.height(500.dp)) {
+                            items(posts!!) { post ->
+                                NewsComponent(post = post, context = context)
                             }
                         }
                     }
 
 
-                    /*end of sample post*/
-
-                    Text(
-                        "Trending",
-                        lineHeight = 16.sp,
-                        fontStyle = FontStyle.Normal,
-                        fontWeight = FontWeight.Bold,
-                        fontFamily = FontFamily(Font(R.font.arial)),
-                        color = Color.Black,
-                        modifier = Modifier.padding(top = 20.dp, bottom = 20.dp)
-                    )
-                    
-                    /*Sample Trending Post*/
-                    
-                    Row(
-                        modifier = Modifier
-                            .fillMaxWidth()
-                            .background(color = Color.White, shape = RoundedCornerShape(10.dp))
-                            .height(129.dp)
-                    ) {
-                            Column(modifier = Modifier.width(200.dp)) {
-                                    Row(modifier = Modifier.padding(horizontal = 15.dp, vertical = 5.dp)) {
-                                        Image(
-                                            painter = painterResource(id = R.drawable.bbc),
-                                            contentDescription = null,
-                                            contentScale = ContentScale.FillHeight,
-                                            modifier = Modifier
-                                                .padding(top = 4.dp, bottom = 1.dp)
-                                                .width(14.dp)
-                                                .height(14.dp)
-                                                .clip(shape = CircleShape)
-
-                                        )
-
-                                        Text(
-                                            "BBC News",
-                                            lineHeight = 15.sp,
-                                            fontStyle = FontStyle.Normal,
-                                            fontWeight = FontWeight.Bold,
-                                            fontFamily = FontFamily(Font(R.font.arial)),
-                                            color = Color(0x4E, 0x4E, 0x4E),
-                                            modifier = Modifier.padding(horizontal = 10.dp, vertical = 1.dp)
-                                        )
-                                    }
-
-                                Text(
-                                    "Facebook Annouces Meta with Virtual Reality and So...",
-                                    lineHeight = 15.sp,
-                                    fontStyle = FontStyle.Normal,
-                                    fontWeight = FontWeight.Bold,
-                                    fontFamily = FontFamily(Font(R.font.arial)),
-                                    color = Color(0x4E, 0x4E, 0x4E),
-                                    modifier = Modifier.padding(horizontal = 15.dp)
-
-                                )
-
-                                Row(Modifier.padding(vertical = 5.dp, horizontal = 20.dp),verticalAlignment = Alignment.CenterVertically) {
-                                    Canvas(
-                                        modifier = Modifier
-                                            .padding(start = 8.dp, end = 8.dp)
-                                            .size(6.dp)
-                                    ) {
-                                        drawCircle(Color(0x4E, 0x4E, 0x4E))
-                                    }
-
-                                    Text(
-                                        "Yesterday",
-                                        lineHeight = 15.sp,
-                                        fontStyle = FontStyle.Normal,
-                                        fontWeight = FontWeight.Bold,
-                                        fontFamily = FontFamily(Font(R.font.arial)),
-                                        color = Color(0x4E, 0x4E, 0x4E),
-                                        modifier = Modifier.padding(horizontal = 10.dp)
-                                    )
-                                }
-
-                            }
-
-                        Spacer(modifier.weight(1f))
-
-                        Image(
-                            painter = painterResource(id = R.drawable.zuck),
-                            contentDescription = null,
-                            contentScale = ContentScale.Crop,
-                            modifier = Modifier
-                                .width(102.dp)
-                                .fillMaxHeight()
-                                .clip(shape = RoundedCornerShape(0.dp, 10.dp, 10.dp, 0.dp))
-                        )
-                    }
+                    /*end of user feed*/
                 }
             }
         }
     }
-}
+
 
 suspend fun SendRequest(OnSuccess: () -> Unit, OnFailure: () -> Unit): List<PostsModel>? {
     val retrofit = RetrofitClient.getInstance()
