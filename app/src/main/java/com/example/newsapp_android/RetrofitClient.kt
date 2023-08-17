b745a9088081a5692cb8b05af6c584637e4575a5
Rquaicoo <russellquaicoo1@gmail.com>
2023-08-17 18:25:11 +0000

    fix loading timeout issue

--- app/src/main/java/com/example/newsapp_android/RetrofitClient.kt
+++ app/src/main/java/com/example/newsapp_android/RetrofitClient.kt
@@ -3,12 +3,20 @@ package com.example.newsapp_android
 import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
 
-object RetrofitClient {
+
+object  RetrofitClient {
+    val okHttpClient: OkHttpClient = OkHttpClient.Builder()
+        .readTimeout(90, TimeUnit.SECONDS)
+        .connectTimeout(90, TimeUnit.SECONDS)
+        .build()
+
     fun getInstance(): Retrofit {
         var retrofit: Retrofit = Retrofit.Builder()
             .baseUrl("https://superficial-vegetable-production.up.railway.app/")
             .addConverterFactory(GsonConverterFactory.create())
+            .client(okHttpClient)
             .build()
         return retrofit
     }
